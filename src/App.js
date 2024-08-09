49f4af9cf25d000820cf462362cd9bd2726939a1
Julieta Aguilera <aguilerajulieta700@gmail.com>
2024-08-09 15:37:23 -0300

    create card

--- src/App.js
+++ src/App.js
@@ -10,23 +10,37 @@ function App() {
   const changeTheme = () => {
     setThemeMode(themeMode === 'light' ? 'dark' : 'light');
   }
+  
 
   return (
-    <Router>
+    
       <div className={`App bg-body-${themeMode}`}>
         <nav className={`navbar navbar-expand-lg navbar-dark bg-dark flex-column ${themeMode}`}>
           <div className="container-fluid">
             <div className="collapse navbar-collapse justify-content-end" id="navbarText">
             </div>
           </div>
+          <a className='nav-link text-white me-3' href='#' onClick={changeTheme}>
+              {themeMode === 'light' ? <i className='bi bi-toggle-off fs-3'></i>:
+              <i className='bi bi-toggle-on fs-3'></i>}
+            </a>
         </nav>
-        <div>
-          <Routes>
-            <Route path='/' element={<Init themeMode={themeMode}/>}/>
-          </Routes>
+
+        <div class="card text-center mb-3" style={{ Width: "18rem" }}>
+            <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
         </div>
-      </div>
-    </Router>
+
+        
+      <Router>
+            <Routes>
+              <Route path='/' element={<Init themeMode={themeMode}/>}/>
+            </Routes>
+      </Router>
+    </div>
   );
 }
 
